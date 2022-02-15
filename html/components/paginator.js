a676057d75f33e286de4623febe2080db791f316
Georges Markosian <gm@admins-MacBook-Pro.local>
2022-02-15 14:28:50 +0300

    Refactor paginator styles

--- html/components/paginator.js
+++ html/components/paginator.js
@@ -9,60 +9,62 @@ export default {
       type: Number,
       required: true,
       default: 1,
-    }
+    },
   },
 
   computed: {
     pages() {
       return Array.from({ length: this.total_pages }, (_, idx) => idx + 1)
     },
-    show_pages() {
+    visible_pages() {
       const firstPage = this.pages[0]
       const lastPage = this.pages.length
+      // @TODO: move to consts.js
       const RANGE = 3
       const SPACE = '...'
 
       if (this.pages.length <= 5) {
         return this.pages
       }
+
+      if (this.current_page <= RANGE + 1) {
+        const restPages = this.pages.slice(firstPage, RANGE + 1)
+
+        return this.current_page === RANGE + 1
+          ? [firstPage, ...restPages, this.current_page + 1, SPACE, lastPage]
+          : [firstPage, ...restPages, SPACE, lastPage]
+      }
       
-      if (this.current_page <= (RANGE + 1)) {
-        const restPages = [...this.pages].slice(firstPage, RANGE + 1)
+      if (this.current_page >= lastPage - RANGE) {
+        const restPages = this.pages.slice(lastPage - RANGE - 1, lastPage - 1)
 
-        return this.current_page === (RANGE + 1) 
-          ? [firstPage, restPages, this.current_page + 1, SPACE, lastPage].flat()
-          : [firstPage, restPages, SPACE, lastPage].flat()
-      } else if (this.current_page >= (lastPage - RANGE)) {
-        const restPages = [...this.pages].slice(lastPage - RANGE - 1, lastPage - 1)
-
-        return this.current_page === (lastPage - RANGE)
-          ? [firstPage, SPACE, this.current_page - 1, restPages, lastPage].flat()
-          : [firstPage, SPACE, restPages, lastPage].flat()
-      } else {
-        return [firstPage, SPACE, this.current_page - 1, this.current_page, this.current_page + 1, SPACE, lastPage]
+        return this.current_page === lastPage - RANGE
+          ? [firstPage, SPACE, this.current_page - 1, ...restPages, lastPage]
+          : [firstPage, SPACE, ...restPages, lastPage]
       }
 
+      return [firstPage, SPACE, this.current_page - 1, this.current_page, this.current_page + 1, SPACE, lastPage]
     },
     is_previous_disabled() {
       return this.current_page === 1
     },
     is_next_disabled() {
       return this.current_page === this.total_pages
-    }
+    },
   },
 
   methods: {
     goToNextPage() {
-      let page = this.current_page < this.total_pages ? (this.current_page + 1) : this.current_page
+      let page = this.current_page < this.total_pages ? this.current_page + 1 : this.current_page
       this.onChangePage(page)
     },
     goToPreviousPage() {
-      let page = this.current_page > 1 ? (this.current_page - 1) : this.current_page
+      let page = this.current_page > 1 ? this.current_page - 1 : this.current_page
       this.onChangePage(page)
     },
     onChangePage(page) {
       this.$emit('onChangePage', page)
-    }
+    },
   },
 
   template: `
@@ -77,7 +79,7 @@ export default {
         <button
         class="button-page"
         :class="{ 'page-active': p === current_page }"
-        v-for="p in show_pages"
+        v-for="p in visible_pages"
         @click="onChangePage(p)"
         :disabled="p === '...' || p === current_page"
         >{{ p }}</button>
